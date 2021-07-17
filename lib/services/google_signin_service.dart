a0a78593bb28dff8975e6d8d07226842dfeed5bb
Cristian Ronda <cristian.ronda@shockoe.com>
2021-07-17 16:31:44 -0500

    feat: add custom verify token

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSigninService {
   static GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -9,7 +12,11 @@ class GoogleSigninService {
   static Future<GoogleSignInAccount?> signInGoogle() async {
     try {
       final account = await _googleSignIn.signIn();
-      print("$account");
+      final googleAuth = await account?.authentication;
+      final verifyToken = Uri.parse("http://10.0.2.2:4321/google");
+      await http.post(verifyToken,
+          headers: {'Content-type': 'application/json'},
+          body: jsonEncode({'tokenId': googleAuth?.idToken}));
       return account;
     } catch (e) {
       print("error signin ---> $e");
